86f1db89b4e9dd6f4681d5192363e25dbaa06961
LotsOfValues <70132687+LotsOfValues@users.noreply.github.com>
2020-08-24 17:46:10 +1000

    epicness
    holy shit

---
+++ ideologies.js
@@ -0,0 +1,587 @@
+ideologies = [
+    {
+        "name": "Self-righteous Cowboy",
+        "stats": {
+            "ther": 0,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "№ of people = № of ideologies",
+        "stats": {
+            "ther": 100,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Meta-Centrism",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+        {
+        "name": "Greguevarianist",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Bullied Kid",
+        "stats": {
+            "ther": 50,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Top Chef",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 0,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Modern-day Politician",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 100,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Universal Center",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Enlightened Centrist",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Just-me-thnostatist",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Sad Loner",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+        {
+        "name": "Ironic Anti-Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Liberal Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Unironic Anti-Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Extremist Spy",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 100,
+            "cord": 0
+        }
+    },
+    {
+        "name": "True Centrist",
+        "stats": {
+            "ther": 25,
+            "belf": 100,
+            "polc": 75,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Boomer Centrist",
+        "stats": {
+            "ther": 0,
+            "belf": 50,
+            "polc": 0,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Weakling Centrist",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Based and Redpilled",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 100,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Hellgriller",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Senatorialist",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Moralist",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 25,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Realist",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Inversive Realist",
+        "stats": {
+            "ther": 0,
+            "belf": 50,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Boring Realist",
+        "stats": {
+            "ther": 50,
+            "belf": 100,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Immoralist",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Occam Realist",
+        "stats": {
+            "ther": 25,
+            "belf": 100,
+            "polc": 50,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Orwellian Realist",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Fishhook theorist",
+        "stats": {
+            "ther": 25,
+            "belf": 50,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Centrist Anti-Centrist",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 75,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Moderate Anti-Centrist",
+        "stats": {
+            "ther": 100,
+            "belf": 100,
+            "polc": 100,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Fake Centrist",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 100,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Radical Centrist",
+        "stats": {
+            "ther": 75,
+            "belf": 0,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Anti-Extremist",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 50,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Apolitical",
+        "stats": {
+            "ther": 0,
+            "belf": 50,
+            "polc": 0,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Radical Apolitical",
+        "stats": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Political Nihilist",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Centrist Sympathiser",
+        "stats": {
+            "ther": 100,
+            "belf": 50,
+            "polc": 100,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Singularity Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 50,
+            "polc": 50,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Extremism",
+        "stats": {
+            "ther": 100,
+            "belf": 50,
+            "polc": 50,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Anti-flairism",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 25,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Normie",
+        "stats": {
+            "ther": 50,
+            "belf": 100,
+            "polc": 0,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Radical Moderate",
+        "stats": {
+            "ther": 0,
+            "belf": 25,
+            "polc": 0,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Aroace Agender Apolitical Atheist",
+        "stats": {
+            "ther": 100,
+            "belf": 25,
+            "polc": 0,
+            "cord": 25
+        }
+    },
+    {
+        "name": "Fence with windows",
+        "stats": {
+            "ther": 75,
+            "belf": 50,
+            "polc": 75,
+            "cord": 25
+        }
+    },
+    {
+        "name": "Contradictionalism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Populism",
+        "stats": {
+            "ther": 100,
+            "belf": 25,
+            "polc": 100,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Extremist Centrism",
+        "stats": {
+            "ther": 100,
+            "belf": 0,
+            "polc": 100,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Gravitational Centrism",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Politicophobe",
+        "stats": {
+            "ther": 25,
+            "belf": 75,
+            "polc": 0,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Status Quo Upholder",
+        "stats": {
+            "ther": 75,
+            "belf": 75,
+            "polc": 50,
+            "cord": 50
+        }
+    },
+    {
+        "name": "Blind Follower",
+        "stats": {
+            "ther": 80,
+            "belf": 100,
+            "polc": 25,
+            "cord": 40
+        }
+    },
+    {
+        "name": "Average Non-voter",
+        "stats": {
+            "ther": 20,
+            "belf": 80,
+            "polc": 20,
+            "cord": 80
+        }
+    },
+    {
+        "name": "Thanosism",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 40,
+            "cord": 20
+        }
+    },
+    {
+        "name": "Peacemaker",
+        "stats": {
+            "ther": 75,
+            "belf": 100,
+            "polc": 30,
+            "cord": 80
+        }
+    },
+    {
+        "name": "Optimistic Centrism",
+        "stats": {
+            "ther": 75,
+            "belf": 100,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+    {
+        "name": "Political Doomer",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 25,
+            "cord": 40
+        }
+    },
+    {
+        "name": "Uninformed Centrism",
+        "stats": {
+            "ther": 25,
+            "belf": 50,
+            "polc": 40,
+            "cord": 80
+        }
+    },
+    {
+        "name": "UK Voter",
+        "stats": {
+            "ther": 60,
+            "belf": 50,
+            "polc": 70,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Inoffensive Centrist Democracy",
+        "stats": {
+            "ther": 75,
+            "belf": 100,
+            "polc": 50,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Someone with opinions",
+        "stats": {
+            "ther": 70,
+            "belf": 35,
+            "polc": 60,
+            "cord": 75
+        }
+    },
+    {
+        "name": "Neocentrism",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Post-centrism",
+        "stats": {
+            "ther": 50,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "name": "Anti-politicism",
+        "stats": {
+            "ther": 0,
+            "belf": 100,
+            "polc": 0,
+            "cord": 50
+        }
+    },
+    {
+        "name": "My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone.",
+        "stats": {
+            "ther": 50,
+            "belf": 50,
+            "polc": 0,
+            "cord": 100
+        }
+    },
+];
---
+++ questions.js
@@ -0,0 +1,362 @@
+questions = [
+    {
+        "question": "Giving lots of power to megacorporations is not much different from giving same ammount of power to the goverment.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+        {
+        "question": "Nazis and communists are basically the same.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Anarcho-(insert_random_ideology_name) is the same as any other type of anarchism.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Totalitarian regimes = literally 1984, no matter the policies.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Anti-fascist and social justice movements are as biggoted as those whom they oppose.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Everyone far-right is a nazi.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Everyone far-left is a stalinist.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "All libs are leftists.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Theists and atheists are equally vocal and annoying.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "question": "Current political status quo is okay, even if it needs minor improvements.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+            {
+        "question": "Whoever wrote the previous question has a major case of dumb.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Technological, cultural and societal progress go hand-in-hand with each other.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "The traditon/progress axis is stupid. Supporting science should not be equated to being pro-lgbt or pro-racial equality. The same goes the other way around.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Extremists and/or a group I dislike deserve to fucking die. That's the only way to achieve peace.",
+        "effect": {
+            "ther": 0,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Authoritarian goverment allows the ones in control to commit serious crimes against people's rights, which is bad, but that issue shouldn't be resolved by the drastic measures of removing authority altogether.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "Saying that all races/cultures are the exact same is wrong, but so is thinking that racial/cultural background is the main factor in ones actions.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+                {
+        "question": "The most drastic measure for political change is a demonstration/protest. Anything more extreme than that is just meaningless violence.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "question": "I would only participate in a protest if I'm sure that things will not get violent.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 0
+        }
+    },
+    {
+        "question": "I would not participate in any kind of protest.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+               {
+        "question": "Serious political media does not interest me.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+                   {
+        "question": "I do not want to vote on any political candidate/party.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+                   {
+        "question": "I know the name of the president, prime minister, chancellor and multiple other politicians of my country.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+  {
+        "question": "I have never learned history/Forgot most of it.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+ {
+        "question": "My knowledge of politics mostly comes from internet memes and/or satirical media.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+ {
+        "question": "I have debated about my political beliefs multiple times.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": 0
+        }
+    },
+    {
+        "question": "Whenever someone talks about something even slightly related to social issues, I tell them to quit being so political or at least think about telling them so.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+    {
+        "question": "If I see a political show and/or news on TV, I change the channel.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+    },
+    {
+        "question": "ONLY ONE of these is correct: Righ-Wingers generally provide more logical and constructive arguments | Left-Wingers are reasonable.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+    },
+        {
+        "question": "ONLY ONE of these is correct: The freer the markets, the freer the people | The freer the people, the freer the markets.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+    },
+       {
+        "question": "ONLY ONE of these is correct: White identarians are not racists | Furries are not gay.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+            {
+        "question": "ONLY ONE of these is correct: Communalism means robbery | Social darwinism means murder.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+          {
+        "question": "ONLY ONE of these is correct: Totalitarism is an unnecessary evil | Anarchy is unnecessary chaos.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+        {
+        "question": "ONLY ONE of these is correct: Most herbivore animals are opportunistic carnivores | Animal cruelty is an issue that needs to be dealt with.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+      {
+        "question": "ONLY ONE of these is correct: Police doesn't need guns to protect people | Citizens don't need guns to protect themselves.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+      {
+        "question": "ONLY ONE of these is correct: I love my country/nation | Most choices my country makes are very questionable.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+      {
+        "question": "ONLY ONE of these is correct: There is no morality without god | If god exists, he's either not omnipotent or not benevolent.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 0,
+            "cord": -10
+        }
+     },
+      {
+        "question": "Whenever I see people arguing, I try to find compromise between both opinions instead of taking sides.",
+        "effect": {
+            "ther": 0,
+            "belf": 10,
+            "polc": 0,
+            "cord": 10
+        }
+     },
+      {
+        "question": "The more axes a political test has, the better.",
+        "effect": {
+            "ther": 10,
+            "belf": -10,
+            "polc": 0,
+            "cord": 0
+        }
+     },
+      {
+        "question": "All modern-day politicians are extremely close to each other in their political beliefs.",
+        "effect": {
+            "ther": -10,
+            "belf": 0,
+            "polc": -10,
+            "cord": 0
+        }
+     },
+      {
+        "question": "Not choosing a lesser evil will give rise to a greater evil.",
+        "effect": {
+            "ther": 0,
+            "belf": 0,
+            "polc": 10,
+            "cord": -10
+        }
+     },
+];
